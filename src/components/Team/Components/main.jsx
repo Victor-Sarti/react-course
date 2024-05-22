474d6fb01d598987acf7d405f41d0ed55495976b
victor-sarti <sartivictor24@gmail.com>
2024-05-21 22:07:09 -0300

    feat: created main container

---
+++ src/components/Team/Components/main.jsx
@@ -0,0 +1,15 @@
+import '../styles.css'
+
+export default function Main(){
+    return(
+        <div className='Main-container'>
+            <h1>We’re hiring!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <button>Open positions</button>
+        </div>
+            
+            
+       
+        
+    )
+}
