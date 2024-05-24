2ccd4a97bd616b01fbf353654715dccbb3794cde
victor-sarti <joselucas3005@gmail.com>
2024-05-23 22:14:51 -0300

    feat: created gallery-composition

---
+++ src/components/Gallery/src/pattern/composition/gallery-text.composition.jsx
@@ -0,0 +1,14 @@
+import "../../../styles.css"
+
+export default function Gallery(){
+    return(
+
+        <div className="gallery-title">
+        <h1>Image Gallery</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+         </div> 
+         
+        
+    )
+}
