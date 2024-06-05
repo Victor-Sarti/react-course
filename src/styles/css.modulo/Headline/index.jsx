4943367164375c1d7fe8e966f7a2fc1bec257828
victor-sarti <sartivictor24@gmail.com>
2024-06-04 22:17:56 -0300

    Creat and Added <Headline/> and <Titulo/> components to the app

---
+++ src/styles/css.modulo/Headline/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./style.module.css"
+
+export default function Headline() {
+    return(
+        <div>
+            <h1 className={styles.titulo}> Descoberta revolucionária: Nova tecnologia 
+                trasnforma lixo plastico em combustível limpo </h1>
+           
+            <p className={styles.descricao}> Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Eaque qui officia ducimus voluptate nobis nulla quis in.
+             Ea enim maiores repudiandae quas. Iusto doloribus iste assumenda autem?
+              Doloribus, ipsum accusamus. </p>
+      
+        </div>
+    )
+}
