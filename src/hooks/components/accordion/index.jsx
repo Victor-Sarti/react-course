c28cdd1f540e620c7867036e838f8f86b46dc0b1
victor-sarti <sartivictor24@gmail.com>
2024-06-05 21:19:34 -0300

    feat: added Accordion Content

--- src/hooks/components/accordion/index.jsx
+++ src/hooks/components/accordion/index.jsx
@@ -1,17 +1,27 @@
 import styled from "styled-components";
-import { AccordionButton, AccordionContainer, AccordionImage, AccordionText } from "./style";
+import { AccordionButton, AccordionContainer, AccordionContent, AccordionImage, AccordionText, AccordionWrapper } from "./style";
 import { AngleUp} from "./src";
 import { AngleDown } from "./src";
 
 export default function Accordion() {
+const isVisible = true
+
     return(
-        <AccordionContainer>
-            <AccordionText> Whats is an accordion component ? </AccordionText>
-            <AccordionButton>
-               <AccordionImage  src={AngleUp}/>
-            </AccordionButton>
-        </AccordionContainer>
+        <AccordionWrapper> 
+            <AccordionContainer>
+                 <AccordionText> Whats is an accordion component ? </AccordionText>
+                 <AccordionButton>
+                    <AccordionImage  src={AngleUp}/>
+                  </AccordionButton>
+             </AccordionContainer>
+      {
+        isVisible && 
+        <AccordionContent>
+            <AccordionText> Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati et commodi corporis, impedit, unde non nulla, repudiandae mollitia fugit blanditiis sequi labore consequatur fugiat quia distinctio qui nisi quasi! Sint ex ducimus nulla facilis. Ullam reprehenderit fugit nulla enim modi? Minima asperiores quibusdam dolorum labore ducimus debitis, eligendi sapiente atque? </AccordionText>
+        </AccordionContent>
+      }
       
+      </AccordionWrapper>
 
     )
 } 
