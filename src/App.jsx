a367f3299e1649971de9a30ba16a13973a50bdf1
victor-sarti <sartivictor24@gmail.com>
2024-06-06 22:04:01 -0300

    feat: added dynamic props in DropdownMenu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
 import "./App.css"
 import DropDownMenu from "./hooks/components/DropDownMenu";
 import { Description, Title } from "./hooks/components/DropDownMenu/style";
+import Navbar11 from "./hooks/components/NavBar11";
 
 function App() {
-   const pages = [
-{
-  Title : "Pages one",
-Description : "Lorem ipsum dolor, sit amet consectetur adipisicing."
-},
-{
-  Title : "Pages one",
-Description : "Lorem ipsum dolor, sit amet consectetur adipisicing."
-},
-{
-  Title : "Pages one",
-Description : "Lorem ipsum dolor, sit amet consectetur adipisicing."
-},
-{
-  Title : "Pages one",
-Description : "Lorem ipsum dolor, sit amet consectetur adipisicing."
-},
-    ]
+//    const pages = [
+// {
+//   Title : "Pages one",
+// Description : "Lorem ipsum dolor, sit amet consectetur adipisicing."
+// },
+// {
+//   Title : "Pages one",
+// Description : "Lorem ipsum dolor, sit amet consectetur adipisicing."
+// },
+// {
+//   Title : "Pages one",
+// Description : "Lorem ipsum dolor, sit amet consectetur adipisicing."
+// },
+// {
+//   Title : "Pages one",
+// Description : "Lorem ipsum dolor, sit amet consectetur adipisicing."
+// },
+//     ]
   
  return (
  
     <body>
-    
-        <DropDownMenu pages={pages}/>
+    <Navbar11/>
+        {/* <DropDownMenu pages={pages}/> */}
     </body>
   );
 }
---
+++ src/hooks/components/NavBar11/index.jsx
@@ -0,0 +1,21 @@
+import *  as S from  "./style"
+
+export default function Navbar11(){
+    return(
+        <S.Container>
+            <S.Logo>LOGO</S.Logo>
+            <S.Nav>
+                <S.Link> Link 1</S.Link>
+                <S.Link> Link 1</S.Link>
+                <S.Link> Link 1</S.Link>
+                <S.Link> Link 1</S.Link>
+               
+                <S.ContainerButton>
+                    <S.Button> Button </S.Button>
+                    <S.Button bgColor="black" > Button </S.Button>
+                </S.ContainerButton>
+                
+            </S.Nav>
+        </S.Container>
+    )
+}
