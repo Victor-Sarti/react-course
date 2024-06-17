685f60a95c30865cb034d072f5e6c10074beb623
victor-sarti <sartivictor24@gmail.com>
2024-06-17 09:43:10 -0300

    feat: added axios framework in project

--- src/styles/tailwindcss/components/home/index.jsx
+++ src/styles/tailwindcss/components/home/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <main >
-      <section className="shadow-2xl shadow-black mt-28 w-96 h-96 flex flex-col items-center justify-center bg-blue-200">
+      <section className="shadow-2xl shadow-black mt-28 w-96 h-96 flex flex-col items-center  justify-center bg-blue-200">
         <h1 className="text-6xl text-black-900">React Course</h1>
         <h2 className="mt-4 text-2xl">by Suporte de Domingo</h2>
       </section>
