1667e40f26816dda69c55c4ca747e5bc27595a8e
victor-sarti <sartivictor24@gmail.com>
2024-06-21 02:01:39 -0300

    feat: I declared sizes and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,jsx,js}"],
   theme: {
     extend: {},
+    colors: {
+      "blue-facebook": '#1877f2',
+      "gray-facebook": '#F0F2F5',
+      "green-facebook": '#36A420',
+      "white-facebook": '#FFFFFF',
+      "gray-custum": '#f7fafc',
+
+    },
+    spacing:{
+      '580': '580px',
+      '380': '380px',
+      '8': '8px',
+      '45': '45px',
+
+    },
   },
   plugins: [],
 }
