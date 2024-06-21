6808351466089a3a689b90951b9cb480c407b1c0
victor-sarti <sartivictor24@gmail.com>
2024-06-21 02:03:57 -0300

    import the facebook in fuction App

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css"
 
 import TeamComposition from "./components/Team"
 
-import Home from "./styles/tailwindcss/components/home"
-import Navbar11 from "./hooks/components/NavBar11";
+ import Home from "./styles/tailwindcss/components/home"
+ import Navbar11 from "./hooks/components/NavBar11";
 
 
 
-import GetPokemon from "./api/api-pokemon";
-import GetCep from "./api/api-cep";
+ import GetPokemon from "./api/api-pokemon";
+ import GetCep from "./api/api-cep";
+import facebook from "./styles/tailwindcss/components/facebook";
 
 
 function App() {
@@ -17,15 +18,17 @@ function App() {
  return (
  
     <body>
-      <Routes>
-          <Route path="/" Component={Home}/>
+       <Routes>
+          <Route path="/" Component={facebook}/>
           <Route path="/teams" Component={TeamComposition} />
           <Route path="/NavBar11" Component={Navbar11} />
           <Route path="/PageTwo" Component={GetPokemon} />
           <Route path="/PageThree" Component={GetCep} />
         
        
-      </Routes>
+      </Routes> 
+
+      
          
     </body>
   );
